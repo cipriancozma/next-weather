5813d8d862c27e2d582d2f619e82c9e245ec35ce
Ciprian <cozma_ciprian19@yahoo.com>
2024-05-29 11:36:04 +0300

    added new components for navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,26 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
+import axios from "axios";
+import { useQuery } from "react-query";
 
 export default function Home() {
+  const { isLoading, error, data } = useQuery("weatherData", async () => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=iasi&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&cnt=56`
+    );
+    return data;
+  });
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center min-h-screen justify-center">
+        <p className="animate-pulse">Loading...</p>
+      </div>
+    );
+  }
+
+  console.log(data);
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
