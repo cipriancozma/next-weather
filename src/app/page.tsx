cda64b028ba4df4f1bc1dadd85c46c26f62aad63
Ciprian <cozma_ciprian19@yahoo.com>
2024-05-29 13:47:22 +0300

    updated packages with date-fns and added the date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Navbar from "@/components/Navbar";
 import axios from "axios";
+import { format, parseISO } from "date-fns";
 import { useQuery } from "react-query";
 
 export default function Home() {
@@ -20,10 +21,25 @@ export default function Home() {
     );
   }
 
+  const dayDate = data?.list[0];
+
   console.log(data);
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar />
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        <section>
+          <div>
+            <h2 className="flex gap-1 text-2xl items-end">
+              <p>{format(parseISO(dayDate?.dt_txt ?? ""), "EEEE")}</p>
+              <p className="text-lg">
+                ({format(parseISO(dayDate?.dt_txt ?? ""), "dd.MM.yyyy")})
+              </p>
+            </h2>
+          </div>
+        </section>
+        <section></section>
+      </main>
     </div>
   );
 }
